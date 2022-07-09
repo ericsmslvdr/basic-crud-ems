316a176011f7855ab19c1a47d568579687814863
ericsslvdr <ericsmsalvador@gmail.com>
2022-07-09 22:18:24 +0800

    Update

--- view/readView.php
+++ view/readView.php
@@ -8,30 +8,36 @@ class ReadView extends Read {
             foreach ($records as $record) {
                 if ($this->isUpdate && $record['id'] === $_POST['empID']) {
                     $rowFname = '<td><input type="text" name="fName"></td>';
-                    $this->rowLname = '<td><input type="text" name="lName"></td>';
-                    $this->rowPass = '<td><input type="text" name="pwd"></td>';
-                    $this->rowAddr = '<td><input type="text" name="addr"></td>';
-                    $this->rowBtns = '<td>
-                                        <input type="submit" class="btn btnCan" name="canBtn" value="Cancel">
-                                        <input type="submit" class="btn btnUpd" name="updBtnConfirm" value="Update">
-                                    </td>';
+                    $rowLname = '<td><input type="text" name="lName"></td>';
+                    $rowPnum = '<td><input type="text" name="pNum"></td>';
+                    $owAddr = '<td><input type="text" name="addr"></td>';
+                    $rowBtns = '<td>
+                                    <input type="submit" class="btn btnCan" name="canBtn" value="Cancel">
+                                    <input type="submit" class="btn btnUpd" name="updBtnConfirm" value="Update">
+                                </td>';
+                } else {
+                    $rowFname = '<td>' . $record['firstName'] . '</td>';
+                    $rowLname = '<td>' . $record['lastName'] . '</td>';
+                    $rowPnum = '<td>' . $record['pNum'] . '</td>';
+                    $rowAddr = '<td>' . $record['address'] . '</td>';
+                    $rowBtns = '<td>
+                                    <a href="./index.php?delID=' . $record['id'] . '"><button type="button" class="btn btnDel">Delete</button></a>
+                                    <input type="submit" class="btn btnUpd" name="updBtn" value="Update">
+                                    <input type="hidden" name="empID" value="' . $record['id'] . '">
+                                    <input type="hidden" name="fName" value="' . $record['firstName'] . '">
+                                    <input type="hidden" name="lName" value="' . $record['lastName'] . '">
+                                    <input type="hidden" name="pass" value="' . $record['pNum'] . '">
+                                    <input type="hidden" name="addr" value="' . $record['address'] . '">
+                                </td>';
                 }
                 echo '<form method="POST">
                         <tr>
                             <td>' . $record['id'] . '</td>
-                            <td>' . $record['firstName'] . '</td>
-                            <td>' . $record['lastName'] . '</td>
-                            <td>' . $record['pNum'] . '</td>
-                            <td>' . $record['address'] . '</td>
-                            <td>
-                                <a href="./index.php?delID=' . $record['id'] . '"><button type="button" class="btn btnDel">Delete</button></a>
-                                <input type="submit" class="btn btnUpd" name="updBtn" value="Update">
-                                <input type="hidden" name="empID" value="' . $record['id'] . '">
-                                <input type="hidden" name="fName" value="' . $record['firstName'] . '">
-                                <input type="hidden" name="lName" value="' . $record['lastName'] . '">
-                                <input type="hidden" name="pNum" value="' . $record['pNum'] . '">
-                                <input type="hidden" name="addr" value="' . $record['address'] . '">
-                            </td>
+                            ' . $rowFname . '
+                            ' . $rowLname . '
+                            ' . $rowPnum . '
+                            ' . $rowAddr . '
+                            ' . $rowBtns . '
                         </tr>
                     </form>';
             }
